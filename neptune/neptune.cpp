aa4ae108352cefb7b7a6754d976fb62618db56dd
Luca Chackalackal <lchackalackal@gmail.com>
2023-02-08 20:19:36 -0600

    began setting up better form semi-pseudocode for neptune.cpp

--- neptune/neptune.cpp
+++ neptune/neptune.cpp
@@ -9,12 +9,12 @@ const unsigned int WIDTH = 1920;
 const unsigned int HEIGHT = 1080;
 
 int main() {
+	/* GLFW WINDOW */
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create window
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Neptune", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "average rice window creator" << std::endl;
@@ -24,68 +24,50 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// Initialize glad
+	/* GLAD */
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "didn't get glad got mad" << std::endl;
 		return -1;
 	}
 
-	Shader sim_shader("square.vert", "rtt.frag");
+	/* SHADER */
 
-	float vertices[] = {
+	/* SCREEN QUAD */
+	float screen_quad[] = {
 		// position		texture
-		 1,	 1,	 0,		1,	1,
-		 1,	-1,	 0,		1,	0,
-		-1,	-1,	 0,		0,	0,
-		-1,	 1,	 0,		0,	1
-	};
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3 
+		 1,	 1,			1,	1,
+		 1,	-1,			1,	0,
+		-1,	 1,			0,	1,
+		-1,	-1,			0,	0,
+		-1,	 1,			0,	1,
+		 1,	-1,			1,	0,
 	};
 
-	/*
-	* VERTEX ARRAY OBJECT: maintained by OpenGL internal state,
-	*		contains vertex data stored in other buffers:
-	*	VERTEX BUFFER OBJECT: vertex data
-	*	ELEMENT BUFFER OBJECT: index data
-	*/
-	unsigned int VAO, VBO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	unsigned int quad_vertex_buffer;
+	glGenBuffers(1, &quad_vertex_buffer);
 
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, quad_vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(screen_quad), screen_quad, GL_STATIC_DRAW);
 
 	// Define data in vertex buffer object
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	// texture attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
 
+	/* FRAMEBUFFER */
+	unsigned int data_framebuffer;
+	glGenFramebuffers(1, &data_framebuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, data_framebuffer);
+	unsigned int draw_buffers[1] = { GL_COLOR_ATTACHMENT0 };
 
-	/* RENDER TO TARGET */
-	unsigned int rtt_buffer;
-	glGenFramebuffers(1, &rtt_buffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, rtt_buffer);
+	/* TEXTURE */
+	unsigned int velocity0, velocity1, dye0, dye1, pressure, divergence;
 
-
-	sim_shader.use();
-	glBindVertexArray(VAO);
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
@@ -93,9 +75,8 @@ int main() {
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	glDeleteBuffers(1, &quad_vertex_buffer);
+	glDeleteFramebuffers(1, &data_framebuffer);
 
 	glfwTerminate();
 	return 0;
