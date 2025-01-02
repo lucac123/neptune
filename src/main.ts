2a71aec5373e46be2b147e63923c913dfbf87d99
Luca Chackalackal <lchackalackal@gmail.com>
2025-01-02 14:11:35 -0600

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/**
+ * Initial entry point
+ */
+function main(): void {
+  console.log("Program execution began");
+}
+
+/**
+ * Run main function after DOM fully loaded
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
