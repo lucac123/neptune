67b5b65b0006052deab578c275911fab133abeb6
Luca Chackalackal <lchackalackal@gmail.com>
2025-01-05 19:39:16 -0600

    implemented view

--- src/view.ts
+++ src/view.ts
@@ -2,8 +2,35 @@
  * view.ts
  *
  * View for the application
+ */
+
+/**
+ * Factory to create and initialize the View.
  *
- * Initializes the appropriate components and HTML elements in the DOM.
+ * @param attachedComponent an arbitrary component to attach to view.
+ */
+export function initView(attachedComponent: HTMLElement): View {
+  // Extract the relevant HTML elements from our web page and ensure they are present and of the correct types
+  const dimensionToggleButton = document.querySelector(
+    "button#dimension-toggle"
+  );
+  if (!(dimensionToggleButton instanceof HTMLButtonElement)) {
+    throw new Error(
+      "Failed to find dimension toggle button, cannot initialize the View"
+    );
+  }
+
+  const componentContainer = document.querySelector("#component-container");
+  if (!(componentContainer instanceof HTMLElement)) {
+    throw new Error("Failed to find container for input web component");
+  }
+
+  componentContainer.append(attachedComponent);
+
+  return new View(dimensionToggleButton);
+}
+
+/**
  * Dispatches events on user interaction.
  * Provides a public interface for modifying the DOM based on user events.
  *
@@ -11,3 +38,45 @@
  * will sit in the DOM and may violate the MVC pattern
  * (used for the simulation render loop)
  */
+class View {
+  // Elements
+  dimensionToggleButton: HTMLButtonElement;
+
+  /**
+   * Constructs a new View instance.
+   *
+   * @param button a button element to use for toggling dimensions
+   */
+  constructor(button: HTMLButtonElement) {
+    this.dimensionToggleButton = button;
+
+    this.dimensionToggleButton.addEventListener(
+      "click",
+      this.handleDimensionButtonClick.bind(this)
+    );
+  }
+
+  /**
+   * Switch dimension button icon with dataset alt
+   */
+  public toggleDimension(): void {
+    const icon = this.dimensionToggleButton.querySelector(
+      "#dimension-toggle-icon"
+    );
+    if (!(icon instanceof HTMLElement)) {
+      throw new Error("Failed to get dimension icon");
+    }
+
+    icon.dataset;
+
+    const alt = icon.dataset.alt ?? "";
+    icon.dataset.alt = icon.getAttribute("icon") ?? "";
+    icon.setAttribute("icon", alt);
+  }
+
+  private handleDimensionButtonClick(): void {
+    const toggleDimensionEvent = new CustomEvent("toggleDimension");
+
+    document.dispatchEvent(toggleDimensionEvent);
+  }
+}
